475c5c0b02cad4ffc2bcc633228fd06c49db2ba3
FrankLafarga <alafarga_24@alu.uabcs.mx>
2026-06-05 11:15:42 -0700

    Commit estable para revisión final con freeDB  5 de junio 2026

--- src/models/Conexion.java
+++ src/models/Conexion.java
@@ -5,9 +5,9 @@ import java.sql.DriverManager;
 
 public class Conexion {
 
-	private static final String URL ="jdbc:mysql://localhost:3306/educadex";
-	private static final String USER ="root";
-	private static final String PASS ="educadex2026";
+	private static final String URL  = "jdbc:mysql://sql.freedb.tech:3306/freedb_rqoJhJV2";
+	private static final String USER = "u_hS7htC";
+	private static final String PASS = "E12EbMHSQrIP";
 
     public static Connection getConnection() {
         Connection con = null;
